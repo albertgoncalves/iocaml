c4dbb9ad8d929fd2a3e3137901ea8ee8e5f326c6
albertgoncalves <goncalves.albert@gmail.com>
2018-12-08 12:31:00 -0500

    Streamline.

--- file_json.ml
+++ file_json.ml
@@ -10,18 +10,17 @@ let string_of_bool_option = function
 
 let () =
     let json = YB.from_file "example.json" in
+    let extr_json util field = extract json util field in
 
-    let title = extract json YBU.to_string "title" in
-    let pages = string_of_int @@ extract json YBU.to_int "pages" in
-    let is_ol =
-        string_of_bool
-        @@ extract json YBU.to_bool "is_online" in
+    let title = extr_json YBU.to_string "title" in
+    let pages = string_of_int @@ extr_json YBU.to_int "pages" in
+    let is_ol = string_of_bool @@ extr_json YBU.to_bool "is_online" in
     let is_tr =
         string_of_bool_option
-        @@ extract json YBU.to_bool_option "is_translated" in
-    let tags = extract json YBU.to_list "tags" |> YBU.filter_string in
+        @@ extr_json YBU.to_bool_option "is_translated" in
+    let tags = extr_json YBU.to_list "tags" |> YBU.filter_string in
 
-    let nested_json = extract json YBU.to_list "authors" in
+    let nested_json = extr_json YBU.to_list "authors" in
     let authors =
         L.map
             (fun author -> extract author YBU.to_string "name") nested_json in
