d78c55cf2e10779a20be0c8414cace198d4ea768
albertgoncalves <goncalves.albert@gmail.com>
2018-12-08 12:26:37 -0500

    Option!

--- file_json.ml
+++ file_json.ml
@@ -13,12 +13,12 @@ let () =
 
     let title = extract json YBU.to_string "title" in
     let pages = string_of_int @@ extract json YBU.to_int "pages" in
+    let is_ol =
+        string_of_bool
+        @@ extract json YBU.to_bool "is_online" in
     let is_tr =
         string_of_bool_option
         @@ extract json YBU.to_bool_option "is_translated" in
-    let is_ol =
-        string_of_bool_option
-        @@ extract json YBU.to_bool_option "is_online" in
     let tags = extract json YBU.to_list "tags" |> YBU.filter_string in
 
     let nested_json = extract json YBU.to_list "authors" in
@@ -26,5 +26,5 @@ let () =
         L.map
             (fun author -> extract author YBU.to_string "name") nested_json in
 
-    let output = (title::pages::is_tr::is_ol::tags) @ authors in
+    let output = (title::pages::is_ol::is_tr::tags) @ authors in
     L.iter (fun str -> print_endline str) output
